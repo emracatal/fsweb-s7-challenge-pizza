b7dec0443a1c8b0a64c879cdf274082bb839436c
emracatal <134175723+emracatal@users.noreply.github.com>
2023-12-09 01:39:40 +0300

    node16

---
+++ build/precache-manifest.bca10f81de9f7945ed8c09c2662f6bd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23dc1785b7d38165aa351a38c595910a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "366aaa2c5326c676b393",
+    "url": "/static/css/main.90e82c98.chunk.css"
+  },
+  {
+    "revision": "e3cfd9abf9d03766670e",
+    "url": "/static/js/2.cc48a7a1.chunk.js"
+  },
+  {
+    "revision": "366aaa2c5326c676b393",
+    "url": "/static/js/main.66af2535.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "28ea8bcf6a93ce68fea218a909445acb",
+    "url": "/static/media/logo.28ea8bcf.svg"
+  },
+  {
+    "revision": "e1143cc0e3fcb007c841e793e7c7047f",
+    "url": "/static/media/mvp-banner.e1143cc0.png"
+  }
+]);
